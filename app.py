481127d517910ec4b7900692bc0485906dd9b0b2
Cyrus Ndung'u Ngugi <139966564+Cyrusz55@users.noreply.github.com>
2024-06-30 15:03:40 +0000

    added connection to database

--- app.py
+++ app.py
@@ -1,41 +1,22 @@
 from flask import Flask, render_template, jsonify
+from database import load_jobs_from_db
+
 app = Flask(__name__)
 
-JOBS=[
-{
-  'id': '1',
-  'title':'keyboardist',
-  'location': 'Kikuyu, Kenya',
-  'salary': 'ksh 20000'
-},
-{
-  'id': '2',
-  'title':'saxophonist',
-  'location': 'Kimbu, Kenya',
-  'salary': 'ksh 19000'
-},
-{
-  'id': '3',
-  'title':'bassist',
-  'location': 'Kisumu, Kenya',
-  'salary': 'ksh 25000'
-},
-{
-  'id': '4',
-  'title':'drummer',
-  'location': 'Nairobi, Kenya',
-  'salary': 'ksh 20000'
-}
-]
+
+
+
 
 
 @app.route("/")
 def hello_world():
-  return render_template('home.html', jobs=JOBS, company_name="CyrusMusic")
+  jobs = load_jobs_from_db()
+  return render_template('home.html', jobs=jobs, company_name="CyrusMusic")
 
 @app.route("/api/jobs")
 def list_jobs():
-  return jsonify(JOBS)
+  jobs = load_jobs_from_db()
+  return jsonify(jobs)
 
 
 if __name__ == "__main__":
--- database.py
+++ database.py
@@ -1,10 +1,37 @@
+<<<<<<< HEAD
 from sqlalchemy import create_engine
 engine = create_engine ("mysql+pymysql://")
 
 
 
 
+=======
+from sqlalchemy import create_engine, text
+import os
+
+
+
+db_connection_str = os.environ['DB_CONNECTION_STRING']
+>>>>>>> 3bb2fdd (added connection to database)
 
 
 
 
+<<<<<<< HEAD
+=======
+
+
+  
+
+
+
+
+  
+def load_jobs_from_db():
+ with engine.connect() as conn:
+   result = conn.execute(text("select * from jobs"))
+   jobs = []
+   for row in result.all():
+     jobs.append(row)
+   return jobs
+>>>>>>> 3bb2fdd (added connection to database)
